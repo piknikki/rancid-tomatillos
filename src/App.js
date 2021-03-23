6805c2082bee10bc2c3b9bddcd019fb80b3c1204
Nikki <piknik@me.com>
2021-03-23 17:42:29 -0600

    Move movies over to component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.png';
 import './App.css';
 import movieData from "./data";
+import Movie from "./Movie";
 
 function App() {
   return (
@@ -8,9 +9,19 @@ function App() {
       <header>
         <img src={logo} className="App-logo" alt="logo" />
         <span className="title">Rancid Tomatillos</span>
-        {movieData.movies.map((movie) => {
-          return <h3>{movie.title}</h3>
-        })}
+        <section className="cards">
+          {movieData.movies.map((movie) => {
+            return <Movie
+              key={movie.id}
+              id={movie.id}
+              poster_path={movie.poster_path}
+              backdrop_path={movie.backdrop_path}
+              title={movie.title}
+              average_rating={movie.average_rating}
+              release_date={movie.release_date}
+            />
+          })}
+        </section>
       </header>
 
     </div>
@@ -27,3 +38,4 @@ export default App;
 //   "average_rating": 6.666666666666667,
 //   "release_date": "2020-09-29"
 // }
+//*<img className="poster" src="https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg" alt="poster" />*/
--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import './Movie.css'
 
-const Movie = () => {
+const Movie = ({ id, poster_path, backdrop_path, title, average_rating, release_date }) => {
   return (
-
+    <article id={id}>
+      <img className="poster" src={poster_path} alt={title} />
+      <p className="movie-title">{title}</p>
+    </article>
   )
 }
 
