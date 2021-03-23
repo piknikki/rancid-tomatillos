0eb5d48941e352e86bb8cdfb63a06f8525081803
Nikki <piknik@me.com>
2021-03-23 16:52:44 -0600

    add basic structure for component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import logo from './logo.png';
 import './App.css';
+import movieData from "./data";
 
 function App() {
   return (
     <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
+      <header>
+        <img src={logo} className="App-logo" alt="logo" />
+        <span className="title">Rancid Tomatillos</span>
+        {movieData.movies.map((movie) => {
+          return <h3>{movie.title}</h3>
+        })}
+      </header>
+
     </div>
   );
 }
 
 export default App;
+
+// {
+//   "id": 694919,
+//   "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
+//   "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
+//   "title": "Money Plane",
+//   "average_rating": 6.666666666666667,
+//   "release_date": "2020-09-29"
+// }
