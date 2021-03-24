c1d6276f39e13bbcfc0452cbd6a457f5acf645ea
Nikki <piknik@me.com>
2021-03-24 07:30:13 -0600

    add remaining info to movie profile

--- src/MovieProfile.js
+++ src/MovieProfile.js
@@ -5,15 +5,14 @@ const MovieProfile = (props) => {
 
   const {   id,
     title,
-    // poster_path,
     backdrop_path,
-    // release_date,
+    release_date,
     overview,
     average_rating,
     genres,
     budget,
-    // revenue,
-    // runtime,
+    revenue,
+    runtime,
     tagline } = props.data
 
   const rottenTomatillos = '🤢'
@@ -21,16 +20,22 @@ const MovieProfile = (props) => {
   return (
     <>
       <p className="ratings">{rottenTomatillos.repeat(Math.floor(average_rating))}</p>
-      <article className="profile-container">
+      <article className="profile-container" id={id}>
         <img className="backdrop" src={backdrop_path} alt={title}/>
         <div className="content-wrapper">
           <h1 className="movie-title">{title}</h1>
           <h2 className="tagline">{tagline}</h2>
           <p className="overview">{overview}</p>
+          <p>
+            <span className="release">Release Date: {release_date} </span>
+            <span className="runtime"> Run time: {runtime}</span>
+          </p>
           <p>{genres.map(genre =>
-            <button className={genre.name.toLowerCase()} type="button">{genre.name}</button>
+            <button className="genre" type="button">{genre.name}</button>
           )}</p>
-          <p>{budget}</p>
+          <p>Budget: {budget}</p>
+          <p>Revenue: {revenue}</p>
+
         </div>
       </article>
     </>
