210d566f958e4a5d21bdedacd7db9237921ed90c
Nikki <piknik@me.com>
2021-03-23 18:04:48 -0600

    Format movie articles

--- src/Movie.js
+++ src/Movie.js
@@ -2,10 +2,15 @@ import React from 'react';
 import './Movie.css'
 
 const Movie = ({ id, poster_path, backdrop_path, title, average_rating, release_date }) => {
+  console.log(Math.floor(average_rating))
+
+  const rottenTomatillos = '🤢'
+
   return (
     <article id={id}>
       <img className="poster" src={poster_path} alt={title} />
-      <p className="movie-title">{title}</p>
+      <p className="movie-title">{rottenTomatillos.repeat(Math.floor(average_rating))}</p>
+      <p className="movie-subtitle">since {release_date}</p>
     </article>
   )
 }
