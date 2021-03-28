0977e7518185d82f0e3c5b82cdeb76991dcfe510
Nikki <piknik@me.com>
2021-03-28 05:54:54 -0600

    Pushing image into css works now

--- src/MovieProfile.js
+++ src/MovieProfile.js
@@ -17,23 +17,28 @@ const MovieProfile = (props) => {
     tagline
   } = props.data
 
-  const rottenTomatillos = '🤢'
+  const tomatillos = '🤢'
 
   const options2 = { style: 'currency', currency: 'USD' };
   const numberFormat2 = new Intl.NumberFormat('en-US', options2);
 
-  const formattedCurrency = (value) => numberFormat2.format(value)
+  const formattedCurrency = (value) => numberFormat2.format(value);
+
+  const styles = {
+
+  }
 
   return (
     <section className="profile">
-      <span className="ratings">{rottenTomatillos.repeat(Math.floor(average_rating))}</span>
+      <span className="ratings">{tomatillos.repeat(Math.floor(average_rating))}</span>
       <article className="profile-container" id={id}>
         {/*<img id="backdrop" className="backdrop" src={backdrop_path} alt={title}/>*/}
         <div id="backdrop" className="backdrop" style={{
-          backgroundImage: `url(${backdrop_path})`
-        }}> </div>
-
-        <div className="content-wrapper">
+          backgroundImage: `url(${backdrop_path})`,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+        }}>
           <h1 className="movie-title">{title}</h1>
           <h2 className="tagline">{tagline}</h2>
           <p className="overview">{overview}</p>
@@ -58,6 +63,32 @@ const MovieProfile = (props) => {
             <i className="fas fa-times"> </i>
           </button>
         </div>
+
+        {/*<div className="content-wrapper">*/}
+          {/*<h1 className="movie-title">{title}</h1>*/}
+          {/*<h2 className="tagline">{tagline}</h2>*/}
+          {/*<p className="overview">{overview}</p>*/}
+          {/*<p>*/}
+          {/*  <span className="release">Release Date: {release_date} </span>*/}
+          {/*  <span className="runtime"> Run time: {runtime}</span>*/}
+          {/*</p>*/}
+
+          {/*{genres ? genres.map(genre =>*/}
+          {/*  <button className="genre" type="button" key={genre}>{genre}</button>*/}
+          {/*) : <p> </p>}*/}
+
+          {/*<p>Budget: {formattedCurrency(budget)}</p>*/}
+          {/*<p>Revenue: {formattedCurrency(revenue)}</p>*/}
+          {/*<Link to={`/`} >*/}
+          {/*  <button className="go-back btn" onClick={() => props.resetCurrentMovie()}>*/}
+          {/*    <i className="fas fa-arrow-left"> </i>*/}
+          {/*  </button>*/}
+          {/*</Link>*/}
+          {/*/!* todo ==> make the delete button a redirect? *!/*/}
+          {/*<button className="delete btn">*/}
+          {/*  <i className="fas fa-times"> </i>*/}
+          {/*</button>*/}
+        {/*</div>*/}
       </article>
     </section>
   )
