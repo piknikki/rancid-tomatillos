707dcec44c52597620c1ac2e9b8623af94920a99
Nikki <piknik@me.com>
2021-04-02 08:26:56 -0600

    Add form logic and convert to class component

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -1,19 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './SearchBar.css'
 
-const SearchBar = () => {
-  return (
-    <div className="searchbar-container">
-      <h1>Welcome.</h1>
-      <h3>We have all the movies. Explore now.</h3>
-      <form>
-        <label>
-          <input type="text" placeholder="Search for a movie......."/>
-        </label>
-        <input className="searchBtn" type="submit" value="Search"/>
-      </form>
-    </div>
-  )
-}
+export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
 
-export default SearchBar
+    this.state = {
+      searchTerm: ''
+    }
+  }
+
+  getSearchTerm = (event) => {
+    event.preventDefault()
+    this.setState({ searchTerm: event.target.value })
+  }
+
+  render() {
+
+    return (
+      <div className="searchbar-container">
+        <h1>Welcome.</h1>
+        <h3>We have all the movies. Explore now.</h3>
+        <form>
+          <label>
+            <input
+              type="text"
+              placeholder="Search for a movie......."
+              onChange={this.getSearchTerm}
+            />
+          </label>
+          <input
+            className="searchBtn"
+            type="submit"
+            value="Search"
+            onClick={this.props.findMovie}
+          />
+        </form>
+      </div>
+    )
+  }
+}
